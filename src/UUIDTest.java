589dccf34d2281fd0b72a7ae97c50b9a227d0484
chenshi03 <chenshi03@58.com>
2020-05-28 10:55:51 +0800

    UUIDTest

---
+++ src/UUIDTest.java
@@ -0,0 +1,11 @@
+import java.util.UUID;
+
+public class UUIDTest {
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++){
+		String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+		System.out.println(uuid);
+		}
+		}
+
+}
